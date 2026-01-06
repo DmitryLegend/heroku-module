b152bb556dc2762ab4697d32eabb0e700511a61f
DmitryLegend <131806843+DmitryLegend@users.noreply.github.com>
2026-01-06 05:27:43 +0400

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,7 +4,6 @@ ROOTFS="/data/local/linux_bot"
 URL="https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/aarch64/alpine-minirootfs-3.18.4-aarch64.tar.gz"
 ARCHIVE="$MODPATH/alpine.tar.gz"
 
-# ШАГ 0: Принудительное создание структуры
 ui_print "- Подготовка путей"
 mkdir -p $ROOTFS
 mkdir -p $ROOTFS/home/heroku
@@ -14,7 +13,7 @@ ui_print "- Загрузка компонентов (1/3) Alpine Linux"
 curl -L -s -o "$ARCHIVE" "$URL"
 
 if [ ! -s "$ARCHIVE" ]; then
-    ui_print "  ! Ошибка: Файл не скачан"
+    ui_print "  ! Ошибка загрузки"
     abort
 fi
 
@@ -23,13 +22,18 @@ tar -xzf "$ARCHIVE" -C $ROOTFS
 rm "$ARCHIVE"
 
 ui_print "- Установка компонентов (3/3) Инструменты Heroku"
-ui_print "  Загрузка пакетов (это может занять время)"
+ui_print "  Установка Python и Git... (ожидайте)"
 echo "nameserver 8.8.8.8" > $ROOTFS/etc/resolv.conf
 
-# Добавили вывод каждой операции, чтобы видеть прогресс
-chroot $ROOTFS /sbin/apk add --no-cache --progress python3 py3-pip git bash curl
+# Мы убрали --progress и добавили -q (quiet), чтобы не было спама строками
+if chroot $ROOTFS /sbin/apk add --no-cache -q python3 py3-pip git bash curl; then
+    ui_print "  Инструменты установлены успешно"
+else
+    ui_print "  ! Ошибка при установке пакетов"
+    abort
+fi
 
-# Фиксация прав в реальной системе
+ui_print "- Финализация прав доступа"
 set_perm_recursive $MODPATH 0 0 0755 0755
 set_perm_recursive $ROOTFS 0 0 0755 0755
 
@@ -41,5 +45,3 @@ ui_print " 1 Перезагрузите устройство "
 ui_print " 2 Зайдите в Magisk "
 ui_print " 3 Нажмите кнопку Action "
 ui_print "****************************************"
-
-sleep 3
