c206aef6ebf1d5db675ca500589297119b7477ec
DmitryLegend <131806843+DmitryLegend@users.noreply.github.com>
2026-01-06 05:35:34 -0500

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,10 +13,12 @@ ui_print "- Распаковка системы"
 tar -xzf "$ARCHIVE" -C $ROOTFS
 rm "$ARCHIVE"
 
-ui_print "- Установка инструментов (gcc/python/git)"
+ui_print "- Установка инструментов (3/3)"
 echo "nameserver 8.8.8.8" > $ROOTFS/etc/resolv.conf
-# Добавили пакеты для сборки библиотек (build-base, python3-dev)
-chroot $ROOTFS /sbin/apk add --no-cache -q python3 py3-pip git bash curl build-base python3-dev libffi-dev
+ui_print "  Загрузка пакетов (это может занять время)..."
+
+# Оставляем --progress для того самого красивого вывода как на скриншоте
+chroot $ROOTFS /sbin/apk add --no-cache --progress python3 py3-pip git bash curl build-base python3-dev libffi-dev
 
 set_perm_recursive $MODPATH 0 0 0755 0755
 set_perm_recursive $ROOTFS 0 0 0755 0755
